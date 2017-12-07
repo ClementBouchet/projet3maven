283137a14c4c57965558dc160fe7487a269f4dbc
ClementBouchet <bouchetclem@gmail.com>
2017-12-07 19:30:05 +0100

    premier commit

---
+++ backEnd/src/main/java/com/projet3/demo/backEnd/App.java
@@ -0,0 +1,10 @@
+
+package com.projet3.demo.backEnd;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ frontEnd/src/main/java/com/projet3/demo/frontEnd/App.java
@@ -0,0 +1,13 @@
+package com.projet3.demo.fronEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
